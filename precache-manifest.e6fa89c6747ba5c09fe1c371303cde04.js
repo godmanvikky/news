810b230973bd1a6e6bd719d4f07f7f1818637b2c
godmanvikky <wish.vignesh95@gmail.com>
2019-11-12 16:20:01 +0530

    Updates

---
+++ precache-manifest.e6fa89c6747ba5c09fe1c371303cde04.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879ed3f0568d868eef759cad025848f6",
+    "url": "/news_redux/index.html"
+  },
+  {
+    "revision": "6f05304dd06227e7dcba",
+    "url": "/news_redux/static/js/2.9ff06f9b.chunk.js"
+  },
+  {
+    "revision": "cc346a1fea4be29e234a",
+    "url": "/news_redux/static/js/main.3f01a2ec.chunk.js"
+  },
+  {
+    "revision": "9210e4f3f3cf9084288f",
+    "url": "/news_redux/static/js/runtime-main.48777fe1.js"
+  }
+]);
